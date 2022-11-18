30143c39dc213c6e12329058a7e95c3f660af3e9
Gabriel Giangi <ggiangi@shipnowinc.com>
2022-11-18 18:40:00 -0500

    Added Page Header UI

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
+import { Button, ButtonGroup } from "@material-ui/core";
 
 function Dashboard() {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-            <Outlet />
-        </div>
-    )
+  return (
+    <div className="flex w-full bg-gray-100 justify-around items-center py-2 mt-4 mr-2">
+      <div className="flex-initial text-3xl pl-3 font-bold">My Books</div>
+      <div className="flex-initial">
+        <ButtonGroup variant="outlined" aria-label="outlined button group">
+          <Button className="px-20">Add Book</Button>
+          <Button className="px-20">Add Shelf</Button>
+        </ButtonGroup>
+      </div>
+      <div className="flex-initial">
+        <Box sx={{ "& > :not(style)": { m: 1 } }}>
+          <FormControl variant="standard">
+            <InputLabel htmlFor="input-with-icon-adornment">Search</InputLabel>
+            <Input
+              id="input-with-icon-adornment"
+              startAdornment={
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </Box>
+      </div>
+    </div>
+  );
 }
 export default Dashboard;
