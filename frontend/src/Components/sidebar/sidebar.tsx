bd99249c0517179611fd6a6a62a7fd95a37df106
Gabriel Giangi <ggiangi@shipnowinc.com>
2022-11-18 12:44:10 -0500

    Updated sidebar UI with OG logo

--- frontend/src/Components/sidebar/sidebar.tsx
+++ frontend/src/Components/sidebar/sidebar.tsx
@@ -1,20 +1,20 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Drawer from '@mui/material/Drawer';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
-import Toolbar from '@mui/material/Toolbar';
-import MenuBookIcon from '@mui/icons-material/MenuBook';
-import FlagIcon from '@mui/icons-material/Flag';
-import MovingIcon from '@mui/icons-material/Moving';
-import SettingsIcon from '@mui/icons-material/Settings';
-import LogoutIcon from '@mui/icons-material/Logout';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Drawer from "@mui/material/Drawer";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MailIcon from "@mui/icons-material/Mail";
+import Toolbar from "@mui/material/Toolbar";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import FlagIcon from "@mui/icons-material/Flag";
+import MovingIcon from "@mui/icons-material/Moving";
+import SettingsIcon from "@mui/icons-material/Settings";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const drawerWidth = 240;
 
@@ -36,17 +36,25 @@ export default function Sidebar(props: Props) {
 
   const drawer = (
     <div>
-      <h1 id="title">Book Project</h1>
+      <img
+        src="logo.png"
+        alt="logo"
+        style={{
+          width: "100%",
+          paddingTop: "50px",
+          paddingLeft: "20px",
+          paddingRight: "20px",
+        }}
+      />
       <Toolbar />
       <List>
-        {['My Books', 'Goals', 'Statistics'].map((text, index) => (
+        {["My Books", "Goals", "Statistics"].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index ==0 && <MenuBookIcon />}
-                {index ==1 && <FlagIcon />}
-                {index ==2 &&<MovingIcon />}
-                
+                {index == 0 && <MenuBookIcon />}
+                {index == 1 && <FlagIcon />}
+                {index == 2 && <MovingIcon />}
               </ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
@@ -54,32 +62,31 @@ export default function Sidebar(props: Props) {
         ))}
       </List>
       <div>
-      <List sx={{position: 'absolute',bottom:0 }}>
-
-        {['Settings', 'Logout'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-              {index ==0 && <SettingsIcon />}
-              {index ==1 && <LogoutIcon />}                
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+        <List sx={{ position: "absolute", bottom: 0, width: "100%" }}>
+          {["Settings", "Logout"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index == 0 && <SettingsIcon />}
+                  {index == 1 && <LogoutIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
       </div>
-      
     </div>
   );
-  const title=document.getElementById('title');
-  if (title !=null){
-    title.style.setProperty('text-align','center')
+  const title = document.getElementById("title");
+  if (title != null) {
+    title.style.setProperty("text-align", "center");
   }
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
 
       <Box
@@ -96,8 +103,11 @@ export default function Sidebar(props: Props) {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
@@ -105,8 +115,11 @@ export default function Sidebar(props: Props) {
         <Drawer
           variant="permanent"
           sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
           open
         >
