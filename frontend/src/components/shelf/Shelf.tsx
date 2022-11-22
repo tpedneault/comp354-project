a05baa03312abcacb124e83e03474fb9bd50302d
James <jamesjglee73@gmail.com>
2022-11-22 18:15:34 -0500

    Set up BookCards and IBook Interface.

--- frontend/src/components/shelf/Shelf.tsx
+++ frontend/src/components/shelf/Shelf.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import axios from "axios";
 import {useQuery } from "@tanstack/react-query";
 import {ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import Grid from '@material-ui/core/Grid';
+import BookCard from '../bookCard';
+import IBook from '../../data/interfaces/IBook'
+
+
 function Shelf() {
     const { isLoading, error, data, isFetching } = useQuery(["repoData"],async () => axios
       .get("https://api.github.com/repos/tannerlinsley/react-query")
@@ -23,10 +28,20 @@ function Shelf() {
   }
   
   return(
-    <div>
-      <h1>{data.name}</h1>
-    </div>
+    <>
+      {data?.pages.filter((p: { data: any[]; }) => p.data.map.name).map((p: { data: IBook[]; }) => p.data.map((book : IBook) => (
+            <Grid item style={{ display: 'flex' }} key={`Exhibit-Card:${book.id}`}>
+              <BookCard
+                title={book.title}
+                subheader={book.author_id}
+                imgSrc={book.cover_url}
+                imgAlt="Book Cover"
+              />
+            </Grid>
+      )))}
+            </>
   );
+
 }
 
 export default Shelf;
