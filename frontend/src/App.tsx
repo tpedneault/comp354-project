a6900b7da3f1ec06b52ee132b036d1a5b410fab4
Gabriel Giangi <ggiangi@shipnowinc.com>
2022-11-22 17:00:52 -0500

    Login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,43 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 //import Login from "./login/Login";
-import {useState} from 'react';
+import { useState } from "react";
 
-import {
-    BrowserRouter,
-    Route,
-    Routes,
-} from "react-router-dom";
-import { QueryClient, QueryClientProvider} from 'react-query';
-import Dashboard from './pages/Dashboard';
-import Goals from './pages/Goals'; 
-import Statistics from './pages/Statistics';
-import Settings from './pages/Settings';
-import Layout from './pages/Layout';
-import Login from './pages/Login';
-import { Logout } from '@mui/icons-material';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Dashboard from "./pages/Dashboard";
+import Goals from "./pages/Goals";
+import Statistics from "./pages/Statistics";
+import Settings from "./pages/Settings";
+import Layout from "./pages/Layout";
+import Login from "./pages/Login";
+import { Logout } from "@mui/icons-material";
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 1000 * 60 * 5,
-        cacheTime: 1000 * 60 * 5,
-      },
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      cacheTime: 1000 * 60 * 5,
     },
-  });
+  },
+});
 
 function App() {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <Routes>
-                  <Route path="Login" element={<Login />}/>
-                  <Route path="Logout" element={<Logout />}/>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Dashboard />} />
-                        <Route path="Goals" element={<Goals />} />
-                        <Route path="Statistics" element={<Statistics />} />
-                        <Route path="Settings" element={<Settings />} />
-                    </Route>
-            
-                </Routes>
-            </BrowserRouter>
-      </QueryClientProvider>
-    );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="Login" element={<Login />} />
+          <Route path="Logout" element={<Logout />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="Goals" element={<Goals />} />
+            <Route path="Statistics" element={<Statistics />} />
+            <Route path="Settings" element={<Settings />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 export default App;
