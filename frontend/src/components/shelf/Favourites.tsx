55b773185e1b4255b744e584faa090988ab0a2e3
Gabriel Giangi <ggiangi@shipnowinc.com>
2022-11-24 15:12:08 -0500

    Updated favourite and changing shelves UI

--- frontend/src/components/shelf/Favourites.tsx
+++ frontend/src/components/shelf/Favourites.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card, CardMedia, Grid } from "@mui/material";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ChangeShelf from "../ChangeShelf";
 
 const shelfData = [
   {
-    isbn: "1234567890",
     img: "cover.webp",
+    title: "The Gravity of Us",
+    isbn: "Breakfast",
     isFavourite: false,
   },
   {
@@ -33,14 +35,16 @@ function Favorites() {
             sx={{ width: 150, position: "relative", margin: "20px" }}
           >
             <CardMedia component="img" height="140" image={book.img} />
+            <div className="bottom-0 bg-blue-50 h-5"></div>
             <Button
               size="medium"
-              sx={{ position: "absolute", bottom: 5, right: 0, left: 0 }}
+              sx={{ position: "absolute", bottom: 0, left: 0 }}
             >
-              <div className="bg-white w-2/3 rounded-full">
+              <div className="bg-blue-50 w-full rounded-full">
                 <FavoriteBorderOutlinedIcon />
               </div>
             </Button>
+            <ChangeShelf />
           </Card>
         ))}
       </Grid>
