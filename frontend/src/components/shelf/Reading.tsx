002f833174794b460f638b18d4239ba859ca6baf
Gabriel Giangi <gabegiangi@gmail.com>
2022-12-05 14:47:33 -0500

    Add files via upload

--- frontend/src/components/shelf/Reading.tsx
+++ frontend/src/components/shelf/Reading.tsx
@@ -8,23 +8,24 @@ import { useGlobalContext } from "../../App";
 import FavoriteBook from "../FavoriteBook";
 import CardActions from "@material-ui/core/CardActions";
 
-
 function Reading() {
-  const {userID} = useGlobalContext();
-  const {refreshNumber} = useGlobalContext();
-  const [reducerValue, forceUpdate] = useReducer(x => x +1,0)
+  const { userID } = useGlobalContext();
+  const { refreshNumber } = useGlobalContext();
+  const [reducerValue, forceUpdate] = useReducer((x) => x + 1, 0);
 
-  const fetchData: () => any = async() =>{
-    const URL =`http://localhost:3001/api/${userID}/shelves/2`;
+  const fetchData: () => any = async () => {
+    const URL = `http://localhost:3001/api/${userID}/shelves/2`;
     const response = await axios.get(URL);
     return response;
-  }
-  const {data,refetch} = useQuery( ["reading"], fetchData,{enabled: false});
+  };
+  const { data, refetch } = useQuery(["reading"], fetchData, {
+    enabled: false,
+  });
 
   useEffect(() => {
     refetch();
-  }, [refetch, refreshNumber])
-  
+  }, [refetch, refreshNumber]);
+
   return (
     <div className="w-full mt-10">
       <h1 className="text-2xl font-bold text-[#0d47a1] mb-4">Reading</h1>
@@ -34,16 +35,22 @@ function Reading() {
           <Card
             id={book.id}
             key={book.id}
-            sx={{ width: 160, position: "relative", margin: "20px" }}
+            sx={{
+              width: 120,
+              height: 200,
+              position: "relative",
+              margin: "20px",
+            }}
           >
-            <CardMedia component="img" height="50" image={book.cover_url} />
-            <div className="bottom-0 bg-blue-50 h-5"></div>
-            <CardActions disableSpacing>
+            <CardMedia
+              component="img"
+              className="h-40"
+              image={book.cover_url}
+            />
+            <div className="inline-flex">
               <FavoriteBook Book={book.id}></FavoriteBook>
-
-              <ChangeShelf onChange={forceUpdate}Book={book.id}/>
-            </CardActions>
-
+              <ChangeShelf onChange={forceUpdate} Book={book.id} />
+            </div>
           </Card>
         ))}
       </Grid>
