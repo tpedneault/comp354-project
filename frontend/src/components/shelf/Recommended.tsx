b50444cb1cb3d6b453509c6002bdd0dad93a1eaf
Gabriel Giangi <gabegiangi@gmail.com>
2022-12-05 14:51:28 -0500

    Add files via upload

--- frontend/src/components/shelf/Recommended.tsx
+++ frontend/src/components/shelf/Recommended.tsx
@@ -9,47 +9,62 @@ import { useGlobalContext } from "../../App";
 import CardActions from "@material-ui/core/CardActions";
 
 function Recommended() {
-  const [reducerValue, forceUpdate] = useReducer(x => x +1,0)
-  const {userID} = useGlobalContext();
-  const {refreshNumber, setRefreshNumber} = useGlobalContext();
+  const [reducerValue, forceUpdate] = useReducer((x) => x + 1, 0);
+  const { userID } = useGlobalContext();
+  const { refreshNumber, setRefreshNumber } = useGlobalContext();
 
-  const recommended: () => any = async() =>{
-    const URL =`http://localhost:3001/api/${userID}/recommendations`;
+  const recommended: () => any = async () => {
+    const URL = `http://localhost:3001/api/${userID}/recommendations`;
     const response = await axios.get(URL);
     return response;
-  }
-  const {data:getRecommended, refetch,isLoading,isError,isSuccess} = useQuery( ["recommended"], recommended,{enabled: false});
-  
+  };
+  const {
+    data: getRecommended,
+    refetch,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useQuery(["recommended"], recommended, { enabled: false });
+
   useEffect(() => {
     refetch();
-  }, [refreshNumber])
+  }, [refreshNumber]);
   console.log(getRecommended);
-  if(isLoading){
-    return <div>loading...</div>
+  if (isLoading) {
+    return <div>loading...</div>;
   }
-  if(isError){
-    return <div>error</div>
+  if (isError) {
+    return <div>error</div>;
   }
-  if(isSuccess){
-    if(getRecommended?.data !== ""){
+  if (isSuccess) {
+    if (getRecommended?.data !== "") {
       return (
         <div className="w-full mt-10">
-          <h1 className="text-2xl font-bold text-[#0d47a1] mb-4">Recommended</h1>
-    
+          <h1 className="text-2xl font-bold text-[#0d47a1] mb-4">
+            Recommended
+          </h1>
+
           <Grid container spacing={3}>
             {getRecommended?.data.map((book: any) => (
               <Card
                 id={book.id}
                 key={book.id}
-                sx={{ width: 160, position: "relative", margin: "20px" }}
+                sx={{
+                  width: 120,
+                  height: 200,
+                  position: "relative",
+                  margin: "20px",
+                }}
               >
-                <CardMedia component="img" height="140" image={book.cover_url} />
-                <div className="bottom-0 bg-blue-50 h-5"></div>
-                <CardActions disableSpacing>
+                <CardMedia
+                  component="img"
+                  className="h-40"
+                  image={book.cover_url}
+                />
+                <div className="inline-flex">
                   <FavoriteBook Book={book.id}></FavoriteBook>
-    
-                  <ChangeShelf onChange={forceUpdate}Book={book.id}/>
-                </CardActions>
+                  <ChangeShelf onChange={forceUpdate} Book={book.id} />
+                </div>
               </Card>
             ))}
           </Grid>
@@ -58,10 +73,7 @@ function Recommended() {
       );
     }
   }
-  return(
-    <div></div>
-  );
+  return <div></div>;
 }
-
 
 export default Recommended;
